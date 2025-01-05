510d593641fcd93c7f215d51d38e7c8832538d6f
BalaGSankar <131224379+BalaGSankar@users.noreply.github.com>
2025-01-05 12:29:07 +0530

    Add files via upload

---
+++ Balaji/12. Global Variable.py
@@ -0,0 +1,26 @@
+x="Balaji"
+
+
+def Myfun():
+    x="Sankar"
+    print("The Main Don is"," "+x)
+Myfun()
+
+print("Supporting Don is"," "+ x)
+
+#Below is the Global keywords used in all
+
+def Myf1():
+    global y
+y=2
+print(y)
+
+Myf1()
+
+print("Here used globally in Outside function",y)
+
+
+
+
+
+
