a2ab35e2523f84d13641ca3ebdc96c69a48a68de
BalaGSankar <131224379+BalaGSankar@users.noreply.github.com>
2025-01-05 22:34:22 +0530

    Add files via upload

---
+++ Balaji/16. Strings.py
@@ -0,0 +1,59 @@
+print('Hello this is single quotes')
+print("Hello this is double quotes")
+
+a="Hello Python"
+b='Hello Java'
+print(a)
+print(b)
+
+b='Balaji'
+print(b[5])
+
+
+
+for y in "Balaji":
+    print(y)
+
+p="Balaji"
+print(len(p))
+
+txt="Hello this is Balaji"
+print("is" in txt)
+
+
+txt="Balaji is the very good boy"
+if "Balaji" in txt:
+    print("Yes Balaji is available in txt")
+
+txt2="Balaji is the brother of gowtham"
+if "Sankar" not in txt2:
+    print("Yes Sankar Not in Text")
+
+g="Balaji"
+print(g[2:5])
+
+b = "Hello, World!"
+print(b[:5])
+
+b = "Hello, World!"
+print(b[2:])
+
+
+L="balaji"
+print(L.upper())
+
+
+L="BALAJI"
+print(L.lower())
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+
+a = "BALAJI IS THE GOOD BOY"
+print(a.split("THE")) 
+
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
